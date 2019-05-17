67971d00db1812634d958f5b9d9e50807b01fdc6
pdb13 <p.calado13@imperial.ac.uk>
2019-05-17 10:38:52 +0100

    First nanogap device and results

--- Example scripts/Vapp_function_script.m
+++ Example scripts/Vapp_function_script.m
@@ -1,9 +1,11 @@
 % Creates a single carrier device and then applies a 20 mV periodic
 % potential for 2 cycles
 
-par.singlecar = pc('input_files/1 layer single carrier.csv');
-
-soleq = equilibrate(par.singlecar);
+% par.singlecar = pc('input_files/1 layer single carrier.csv');
+% 
+% sol_ini = equilibrate(par.singlecar);
+sol_ini = soleq.ng15.ion;
+par = soleq.ng15.ion.par
 
 % tmax is the period (seconds)
 tmax = 1e-2;
@@ -13,13 +15,13 @@ coeff = [1, Nperiods*(2*pi)/tmax,0];
 Vapp_func = @(coeff, t) coeff(1)*sin(coeff(2)*t + coeff(3));
 
 % Vapp_function(sol_ini, Vapp_func, tmax, tpoints, logtime)
-sol_Vapp_func = Vapp_function(soleq.ion, Vapp_func, coeff, tmax, 200, 0);
+sol_Vapp_func = Vapp_function(sol_ini, Vapp_func, coeff, tmax, 200, 0);
 
 % Plot outputs
 dfplot.Vappt(sol_Vapp_func)
 % Current at mid-point
-dfplot.Jt(sol_Vapp_func, round(par.singlecar.pcum(end)/2))
+dfplot.Jt(sol_Vapp_func, round(par.pcum(end)/2))
 % JV plot
-dfplot.JVapp(sol_Vapp_func, round(par.singlecar.pcum(end)/2))
+dfplot.JVapp(sol_Vapp_func, round(par.pcum(end)/2))
 % Energy level diagrams at t=0 and max amplitude
 dfplot.ELx(sol_Vapp_func, [0, tmax/(4*Nperiods)]);
