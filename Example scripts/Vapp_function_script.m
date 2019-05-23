0efe58ada5075c73c9cb3f9082e72651d83c78f8
pdb13 <p.calado13@imperial.ac.uk>
2019-05-23 12:21:30 +0100

    Added sawtooth function

--- Example scripts/Vapp_function_script.m
+++ Example scripts/Vapp_function_script.m
@@ -1,7 +1,6 @@
 % Creates a single carrier device and then applies a 20 mV periodic
 % potential for 2 cycles
-
-par = pc('input_files/memristor.csv');
+par = pc('input_files/1 layer Nanogap15nm.csv');
 
 soleq = equilibrate(par);
 
@@ -12,23 +11,26 @@ soleq = equilibrate(par);
 % Vapp_func = @(coeff, t) coeff(1)*sin(coeff(2)*t + coeff(3));
 %
 
-tmax = 4;
+tmax = 1e-2;
 Nperiods = 1;   % Number of periods
-coeff = [2.5, Nperiods*(2*pi)/tmax,0];
+coeff = [2, Nperiods*(2*pi)/tmax,0];
 %Vapp_func = @(coeff, t) coeff(1)*sin(coeff(2)*t + coeff(3));
 Vapp_func = @(coeff, t) coeff(1)*sawtooth((t*coeff(2)+0.5*pi),0.5);
 
 % Vapp_function(sol_ini, Vapp_func, tmax, tpoints, logtime)
-sol_Vapp_func = Vapp_function(sol_ini, Vapp_func, coeff, tmax, 400, 0);
+sol_Vapp = Vapp_function(soleq.ion, Vapp_func, coeff, tmax, 200, 0);
+
+% Reproduce Vapp for plotting
+Vapp = Vapp_func(coeff, sol_Vapp.t);
 
 % Plot outputs
-dfplot.Vappt(sol_Vapp_func)
+dfplot.Vappt(sol_Vapp)
 % Current at mid-point
-dfplot.Jt(sol_Vapp_func, round(par.pcum(end)/2))
+dfplot.Jt(sol_Vapp, par.pcum(end))
 % JV plot
-dfplot.JVapp(sol_Vapp_func, round(par.pcum(end)/2))
+dfplot.JVapp(sol_Vapp, par.pcum(end))
 % Mod log J vs V
-dfplot.logJVapp(sol_Vapp_func, round(par.pcum(end)/2))
-
+dfplot.logJVapp(sol_Vapp, par.pcum(end))
 % Energy level diagrams at t=0 and max amplitude
-dfplot.ELx(sol_Vapp_func, [0, tmax/(4*Nperiods)]);
+dfplot.ELx(sol_Vapp, [0, tmax/(4*Nperiods)]);
+
