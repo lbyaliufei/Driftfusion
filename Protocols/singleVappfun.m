36874e45b5b770164224d4a6b83192705d712443
pdb13 <p.calado13@imperial.ac.uk>
2019-05-28 19:38:17 +0100

    moved Vappfun

--- Protocols/singleVappfun.m
+++ Protocols/singleVappfun.m
@@ -1,22 +1,23 @@
-function sol_Vapp = singleVappfun(t_period, N_period) % Creates a single carrier device and then applies a 20 mV periodic
+function sol_Vapp = singleVappfun(sol_ini, t_period, N_period) % Creates a single carrier device and then applies a 20 mV periodic
 % potential for 2 cycles
-par = pc('input_files/1 layer Nanogap15nm base.csv');
-
-soleq = equilibrate(par);
+% par = pc('input_files/1 layer Nanogap15nm base.csv');
+% 
+% soleq = equilibrate(par);
 
 % tmax is the period (seconds)
 % tmax = 10;
 % N_period = 4;   % Number of periods
 % coeff = [1, N_period*(2*pi)/tmax,0];
 % Vapp_func = @(coeff, t) coeff(1)*sin(coeff(2)*t + coeff(3));
+par = sol_ini.par;
 
 tmax = t_period*N_period;
-coeff = [2, (2*pi)/t_period,0];
+coeff = [0.5, (2*pi)/t_period,0,1];
 %Vapp_func = @(coeff, t) coeff(1)*sin(coeff(2)*t + coeff(3));
-Vapp_func = @(coeff, t) coeff(1)*sawtooth((t*coeff(2)+0.5*pi),0.5);
+Vapp_func = @(coeff, t) (coeff(4)+coeff(1)*sawtooth((t*coeff(2)+0.5*pi),0.5));
 
 % Vapp_function(sol_ini, Vapp_func, tmax, tpoints, logtime)
-sol_Vapp = Vapp_function(soleq.ion, Vapp_func, coeff, tmax, 200, 0);
+sol_Vapp = Vapp_function(sol_ini, Vapp_func, coeff, tmax, 200, 0);
 
 % Reproduce Vapp for plotting
 Vapp = Vapp_func(coeff, sol_Vapp.t);
